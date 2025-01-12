a06db74b7ff17f63081b64aaeffc79117aa4c43f
Cristina Constantin <constantin.cristina2002@gmail.com>
2025-01-12 13:04:24 +0200

    stage4

---
+++ Models/ViewModels/UserIndexData.cs
@@ -0,0 +1,10 @@
+﻿using FlavorFusion.Models;
+
+namespace FlavorFusion.ViewModels
+{
+    public class UserIndexData
+    {
+        public IEnumerable<User> Users { get; set; } = new List<User>();
+        public IEnumerable<Recipe> Recipes { get; set; } = new List<Recipe>();
+    }
+}
--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FlavorFusion.Data;
 using FlavorFusion.Models;
+using FlavorFusion.ViewModels;
 
 namespace FlavorFusion.Pages.Users
 {
@@ -21,9 +22,23 @@ namespace FlavorFusion.Pages.Users
 
         public IList<User> User { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public UserIndexData UserData { get; set; } = new UserIndexData();
+        public int UserID { get; set; }
+        public int RecipeID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? recipeID)
         {
-            User = await _context.User.ToListAsync();
+            UserData.Users = await _context.User
+                .Include(u => u.Recipes)
+                .OrderBy(u => u.UserName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                UserID = id.Value;
+                User user = UserData.Users.Where(u => u.Id == id.Value).Single();
+                UserData.Recipes = user.Recipes;
+            }
         }
     }
 }
